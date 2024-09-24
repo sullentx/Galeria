9afb66241fd3a4ef8c7634fd5fb022cbe03cecf3
sullentx <233290@ids.upchiapas.edu.mx>
2024-09-23 23:16:18 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./Components/footer/footer.component";
+import { HeaderComponent } from "./Components/header/header.component";
+import { DashboardComponent } from "./Components/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
